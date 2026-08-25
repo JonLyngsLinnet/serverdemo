37801d021a48fb01cdfc7af721e47a7a74ed9522
jonlinnet28 <jonlinnet28@gmail.com>
2026-08-25 09:21:04 +0200

    second update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
+
+
+public class FlaeskestegController : ControllerBase
+{
+    [HttpGet(nameof(GetFlaeskestegs))]
+    
+    public object GetFlaeskestegs()
+    {
+        return new
+        {
+            id = 1,
+            Name = "umuts flæskesteg"
+        };
+    }
+}
