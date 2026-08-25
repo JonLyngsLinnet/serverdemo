edaa5355d7ab327b9355c43f8a55f91e989df2f7
jonlinnet28 <jonlinnet28@gmail.com>
2026-08-25 10:25:17 +0200

    Allow Any

--- FlaeskestegController.cs
+++ FlaeskestegController.cs
@@ -5,19 +5,35 @@ namespace serverdemo;
 public class FlaeskestegController : ControllerBase
 {
     [HttpGet(nameof(GetFlaeskestegs))]
-    
     public List<Flaeskesteg> GetFlaeskestegs()
     {
         return FlaeskestegsDb.Flaeskestegs;
     }
 
     [HttpPost(nameof(CreateFlaeskesteg))]
-    public void CreateFlaeskesteg([FromQuery] string sproedhed)
+    public void CreateFlaeskesteg([FromQuery]string sproedhed)
     {
         FlaeskestegsDb.Flaeskestegs.Add(new Flaeskesteg()
         {
-            Sproedhed = Int32.Parse(sproedhed), 
+            Sproedhed = Int32.Parse(sproedhed),
             SmagerGodt = true
         });
     }
+    
+    //Update by ID
+    [HttpPatch(nameof(UpdateFlaeskesteg))]
+    public void UpdateFlaeskesteg(int id)
+    {
+        //Update the flaeasskawteafstefg
+    }
+    
+    
+    //Delete by ID
+    [HttpDelete(nameof(DeleteFlaeskesteg))]
+    public void DeleteFlaeskesteg(int id)
+    {
+        //Go to the db and delete by ID
+    }
+    
+    
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApiDocument();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 app.MapControllers();
 app.UseOpenApi();
 app.UseSwaggerUi();
+app.UseCors(_ => _.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+    .SetIsOriginAllowed(_ => true));
 
 app.Run();
